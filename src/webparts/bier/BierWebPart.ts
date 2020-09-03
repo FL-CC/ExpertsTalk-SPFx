bb8ff171ad00de3074f61b09ab0e8ddf74b5be84
Felix Leipold <felix.leipold@computacenter.com>
2020-09-03 08:45:13 +0200

    pnp setup

--- src/webparts/bier/BierWebPart.ts
+++ src/webparts/bier/BierWebPart.ts
@@ -1,29 +1,34 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 
-import * as strings from 'BierWebPartStrings';
-import Bier from './components/Bier';
-import { IBierProps } from './components/IBierProps';
+import * as strings from "BierWebPartStrings";
+import Bier from "./components/Bier";
+import { IBierProps } from "./components/IBierProps";
+import { sp } from "@pnp/sp/presets/all";
 
 export interface IBierWebPartProps {
   description: string;
 }
 
-export default class BierWebPart extends BaseClientSideWebPart<IBierWebPartProps> {
+export default class BierWebPart extends BaseClientSideWebPart<
+  IBierWebPartProps
+> {
+  public onInit(): Promise<void> {
+    return super.onInit().then((_) => {
+      sp.setup({ spfxContext: this.context });
+    });
+  }
 
   public render(): void {
-    const element: React.ReactElement<IBierProps> = React.createElement(
-      Bier,
-      {
-        description: this.properties.description
-      }
-    );
+    const element: React.ReactElement<IBierProps> = React.createElement(Bier, {
+      description: this.properties.description,
+    });
 
     ReactDom.render(element, this.domElement);
   }
@@ -33,7 +38,7 @@ export default class BierWebPart extends BaseClientSideWebPart<IBierWebPartProps
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -41,20 +46,20 @@ export default class BierWebPart extends BaseClientSideWebPart<IBierWebPartProps
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
